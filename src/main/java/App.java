c541a534f98675dff217deb342776836ff46e088
seravifer <sergiavilafernandez@gmail.com>
2018-04-24 22:21:43 +0200

    Added root page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,21 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/root.fxml"));
+        primaryStage.setTitle("Cruz Roja");
+        primaryStage.setScene(new Scene(root, 900, 720));
+        primaryStage.show();
+    }
+
 }
