f320352c966f1e43df50491eb987afba5f992603
seravifer <sergiavilafernandez@gmail.com>
2018-06-09 01:12:43 +0200

    Security refactor

--- src/main/java/utils/Security.java
+++ src/main/java/utils/Security.java
@@ -10,46 +10,41 @@ import java.security.spec.KeySpec;
 import java.util.Base64;
 
 public class Security {
-    private static Cipher ecipher;
-    private static Cipher dcipher;
+
     private static int iterationCount = 10;
     private static String secretKey = "3st03SuN4c0ntr4s3NYa";
-    static byte[] salt = {
+    private static String instance = "PBEWithMD5AndDES";
+    private static byte[] salt = {
             (byte) 0xA9, (byte) 0x9B, (byte) 0xC8, (byte) 0x32,
             (byte) 0x56, (byte) 0x35, (byte) 0xE3, (byte) 0x03
     };
 
-
     public static String encrypt(String plainText) throws Exception {
-        //Key generation for enc and desc
         KeySpec keySpec = new PBEKeySpec(secretKey.toCharArray(), salt, iterationCount);
-        SecretKey key = SecretKeyFactory.getInstance("PBEWithMD5AndDES").generateSecret(keySpec);
-        // Prepare the parameter to the ciphers
+        SecretKey key = SecretKeyFactory.getInstance(instance).generateSecret(keySpec);
         AlgorithmParameterSpec paramSpec = new PBEParameterSpec(salt, iterationCount);
 
-        //Enc process
-        ecipher = Cipher.getInstance(key.getAlgorithm());
-        ecipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
-        String charSet = "UTF-8";
-        byte[] in = plainText.getBytes(charSet);
-        byte[] out = ecipher.doFinal(in);
+        Cipher cipher = Cipher.getInstance(key.getAlgorithm());
+        cipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
+
+        byte[] in = plainText.getBytes("UTF-8");
+        byte[] out = cipher.doFinal(in);
 
         return new String(Base64.getEncoder().encode(out));
     }
 
     public static String decrypt(String encryptedText) throws Exception {
-        //Key generation for enc and desc
         KeySpec keySpec = new PBEKeySpec(secretKey.toCharArray(), salt, iterationCount);
-        SecretKey key = SecretKeyFactory.getInstance("PBEWithMD5AndDES").generateSecret(keySpec);
-        // Prepare the parameter to the ciphers
+        SecretKey key = SecretKeyFactory.getInstance(instance).generateSecret(keySpec);
         AlgorithmParameterSpec paramSpec = new PBEParameterSpec(salt, iterationCount);
-        //Decryption process; same key will be used for decr
-        dcipher = Cipher.getInstance(key.getAlgorithm());
-        dcipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
-        byte[] enc = Base64.getDecoder().decode(encryptedText);
-        byte[] utf8 = dcipher.doFinal(enc);
-        String charSet = "UTF-8";
 
-        return new String(utf8, charSet);
+        Cipher cipher = Cipher.getInstance(key.getAlgorithm());
+        cipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
+
+        byte[] enc = Base64.getDecoder().decode(encryptedText);
+        byte[] utf8 = cipher.doFinal(enc);
+
+        return new String(utf8, "UTF-8");
     }
+
 }
